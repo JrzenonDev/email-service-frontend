5ab9a29a33bdd88931c2ae0b378285b0113f3d5d
JrzenonDev <jrobertoonb@gmail.com>
2023-10-04 00:46:07 -0300

    feat: add components Form and ProductDetail in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import { Spacer } from "@chakra-ui/react";
+import { Form } from "./components/Form/Form";
+import { ProductDetail } from "./components/ProductDetail/ProductDetail";
 
 function App() {
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <div className="container">
+      <Form />
+
+      <Spacer width="6" maxWidth="6" />
+
+      <ProductDetail />
+    </div>
   );
 }
 
